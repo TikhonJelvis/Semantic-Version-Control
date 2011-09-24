fc69c8e480f4d7ba9cfd316def45955a13e28ef7
Tikhon Jelvis <tikhon.jelvis@gmail.com>
2011-09-23 19:53:09 -0700

    Added a help message for the parse command. Now to actually write it...

--- parser/cow.hs
+++ parser/cow.hs
@@ -6,24 +6,30 @@ import Here (here)
 import System.Environment
 
 -- The multiline string stuff confuses Emacs :(
-usage :: IO ()
-usage = putStr [$here|
-Cow - the semantic version control system!
+usage :: String
+usage =
+  [$here|Cow - the semantic version control system!
 
 usage: cow <command>
 where <command> is one of:
   - help
 
-For more information on the commands, use cow help.
+For more information on the commands, use cow help <command>.
 |]
 
-help :: [String] -> IO ()
+help :: [String] -> String
 help []             = help ["help"]
-help (command:rest) = putStr $ case command of
-  "help" -> [$here|
-usage: cow help <command>
+help (command:rest) = case command of
+  "help" ->
+    [$here|usage: cow help <command>
 
 Prints a help message about the specified command.
+|]
+  "parse" ->
+    [$here|usage: cow parse <file> [<output>]
+
+Parses the given file and writes it to the <output> path. If <output>
+is not specified, writes it to <file>.json.
 |]
   otherwise -> "Cow help: Unknown command!\n"
 
@@ -32,6 +38,6 @@ main = getArgs >>= execute
 
 -- TODO: Have this actually do stuff!
 execute :: [String] -> IO ()
-execute []           = usage
+execute []           = putStr usage
 execute (command:args) = case command of
-  "help" -> help args
+  "help" -> putStr $ help args
