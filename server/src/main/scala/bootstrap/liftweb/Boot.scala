8e6099f70bc57fbd3a7de1922eacb1c30bb031c8
Ankur Dave <ankurdave@gmail.com>
2011-09-24 12:38:03 -0700

    Enable static files in server

--- server/src/main/scala/bootstrap/liftweb/Boot.scala
+++ server/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -20,7 +20,30 @@ class Boot {
     // where to search snippet
     LiftRules.addToPackages("code")
 
-    LiftRules.statelessDispatchTable.append(RestHandler)
+    // Build SiteMap
+    def sitemap() = SiteMap(
+      Menu.i("Home") / "index" // the simple way to declare a menu
+      )
 
+    // set the sitemap.  Note if you don't want access control for
+    // each page, just comment this line out.
+    LiftRules.setSiteMapFunc(sitemap)
+
+    //Show the spinny image when an Ajax call starts
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    
+    // Make the spinny image go away when it ends
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    // Use HTML5 for rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))    
+
+    LiftRules.statelessDispatchTable.append(RestHandler)
   }
 }
